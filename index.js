af2afad9e7c7677aa2ec83f26c9fee60aa9f0904
Yashwant Rautela <yashwant.rautela@intelligaia.com>
2020-12-08 11:44:29 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Koa = require('koa');
-const app = new Koa();
-require('dotenv').config(); 
-const port = process.env.PORT||4000;
-app.use(async ctx => {
-  ctx.body = {message:'Hello World'};
-});
+const koa = require('koa');
+const app = new koa();
+require('dotenv').config();
+const port = process.env.PORT;
+app.use(async ctx =>{
+  ctx.body = {message: "Hello World"}
+})
 
 app.listen(port, ()=>{
   console.log(`Server started at port: ${port}`)

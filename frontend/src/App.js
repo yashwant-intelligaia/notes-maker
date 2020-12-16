2d830799fccaf0f235e7d3551ff77b1572bcb9bd
Yashwant Rautela <yashwant.rautela@intelligaia.com>
2020-12-16 08:52:04 +0530

    error state issue resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Home from './component/Home';
 import Note from './component/Note';
@@ -7,36 +8,80 @@ import PageNotFound from './component/PageNotFound'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-
+import { ApolloClient, InMemoryCache, gql, useMutation } from '@apollo/client';
+const CREATE_NOTE = gql`
+    mutation createNote($title: String!){
+        create_note(noteInfo:{title:$title}){
+            status
+            errors{
+                message
+            }
+            note{
+                title
+                _id
+            }
+        }
+    }
+`;
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache()
+});
 const useStyles = makeStyles({
   root: {
-      minWidth: 275,
+    minWidth: 275,
   },
   bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
   },
   title: {
-      fontSize: 14,
+    fontSize: 14,
   },
   pos: {
-      marginBottom: 12,
+    marginBottom: 12,
   },
-  fullwidth:{
-      width: "100%"
+  fullwidth: {
+    width: "100%"
   }
 });
 
 function App() {
+  const [noteError, updateNoteError] = useState(false);
+  const [open, setDialogOpen] = useState(false);
+  const [titleChanged, setTitleChanged] = useState(false);
+  const handleDialogOpen = () => { setDialogOpen(true) };
+  const handleDialogClose = () => { setDialogOpen(false) };
+  const [noteErrorText, updateNoteErrorText] = useState("");
+  var [createNote, { loading, error, data }] = useMutation(CREATE_NOTE, { client: client });
+  const handleChangeOnTitle = () => { updateNoteError(false); updateNoteErrorText(""); setTitleChanged(true);}
   const classes = useStyles();
+  const handleAddNote = async(title) => {
+    setTitleChanged(false);
+    await createNote({ variables: { title: title } });
+    // if(data){
+    //   updateNoteErrorText(data.create_note.status === 201?"":data.create_note.errors[0].message);
+    //   updateNoteError(data.create_note.errors.length>0?true:false);
+    //   setDialogOpen(data.create_note.status === 201?false:true);
+    // }
+  }
+
+  useEffect(() => {
+    if(data){
+      updateNoteErrorText((data.create_note.status === 200 && data.create_note.errors.length>0 && !titleChanged)?data.create_note.errors[0].message:"");
+      updateNoteError((data.create_note.errors.length>0 && !titleChanged)?true:false);
+      setDialogOpen(data.create_note.status === 201?false:true);
+    }
+  })
+  // console.log(data);
   return (
     <Card className={classes.root} variant="outlined">
       <CardContent>
         <Router>
           <Switch>
             <Route exact path="/" component={Home}></Route>
-            <Route exact path="/note/list" component={NoteList}></Route>
+            <Route exact path="/note/list" ><NoteList dialog={open} dialogClose={handleDialogClose} dialogOpen={handleDialogOpen} addNote={handleAddNote} error={noteError} errorText={noteErrorText} titleOnChange={handleChangeOnTitle} /></Route>
             <Route exact path="/test/:title" component={Test}></Route>
             <Route path="/note/:title" component={Note}></Route>
             <Route component={PageNotFound}></Route>
--- frontend/src/component/NoteList.js
+++ frontend/src/component/NoteList.js
@@ -1,47 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import React, { useState } from 'react';
+import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client';
 import { useHistory } from 'react-router-dom';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import NoteIcon from '@material-ui/icons/Note';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Grid from '@material-ui/core/Grid';
+import Tooltip from '@material-ui/core/Tooltip';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+const GET_NOTES = gql`
+  query notes{
+    notes{
+      _id
+      title
+    }
+  }
+`;
+const client = new ApolloClient({
+    uri: 'http://localhost:3000/graphql',
+    cache: new InMemoryCache()
+});
 
 function NoteList(props) {
-    const [noteList, changeNoteList] = useState([]);
-    const [checked, changeChecked] = useState(false);
     const history = useHistory();
-
-    const handleNoteClick = (value) => {
-        history.push(`/note/` + value);
-    }
-
-    useEffect(async () => {
-        if (!checked) {
-            changeChecked(true);
-            const client = new ApolloClient({
-                uri: 'http://localhost:3000/graphql',
-                cache: new InMemoryCache()
-            });
-            await client.query({
-                query: gql`{
-                    notes{
-                        _id
-                        title
-                    }
-                }`,
-            }).then(result => { changeNoteList(result.data.notes) });
-        }
+    const [title, changeNoteTitle] = useState("");
+    const handleChangeTitle = (event) => { changeNoteTitle(event.target.value) }
+    const handleNoteClick = (value) => { history.push(`/note/` + value) }
+    const { loading, error, data } = useQuery(GET_NOTES, {
+        client: client
     });
 
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
     return (
         <React.Fragment>
-            <h1>NoteList</h1>
-            <ul>
-                {noteList.map(value => {
-                    return <li key={value._id}><a onClick={() => handleNoteClick(value.title)}>{value.title}</a></li>
+            <Grid container spacing={3}>
+                <Grid item xs={6} sm={2}>
+                    <h1>NoteList</h1>
+                </Grid>
+                <Grid item xs={6} sm={10}>
+                    <Tooltip title="Add" placement="left-start">
+                        <Fab color="primary" aria-label="add" onClick={props.dialogOpen}>
+                            <AddIcon />
+                        </Fab>
+                    </Tooltip>
+                </Grid>
+            </Grid>
+            <List component="nav" aria-label="main mailbox folders">
+                {data.notes.map(value => {
+                    return (
+                        <ListItem key={value._id} button onClick={() => handleNoteClick(value.title)}>
+                            <ListItemIcon>
+                                <NoteIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={value.title} />
+                        </ListItem>
+                    )
                 })}
-            </ul>
-            <Fab color="primary" aria-label="add">
-                <AddIcon />
-            </Fab>
+            </List>
+
+            <Dialog open={props.dialog} onClose={props.handleDialogClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Add Note</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="title"
+                        label="Title"
+                        type="text"
+                        fullWidth
+                        autoComplete="off"
+                        value={title}
+                        error={props.error}
+                        helperText={props.errorText}
+                        onChange={handleChangeTitle}
+                        onKeyUp={props.titleOnChange}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={props.dialogClose} color="primary">Cancel</Button>
+                    <Button onClick={()=>{props.addNote(title)}} color="primary" disabled={title === ""}>Add</Button>
+                </DialogActions>
+            </Dialog>
         </React.Fragment>
     );
 }
